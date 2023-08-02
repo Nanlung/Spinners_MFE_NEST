4bf08a6c69c05feb34a650c94be06c78119ec4f0
Nanlung <nanlung.temlong@gmail.com>
2023-08-02 11:29:58 +0100

    cart

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import "./index.scss";
 
-import SafeComponent from "./SafeComponent";
 import Header from "home/Header";
 import Footer from "home/Footer";
 import PDPContent from "./PDPContent";
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <SafeComponent>
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
       <Header />
-    </SafeComponent>
-    <div className="my-10">
-      <PDPContent />
-    </div>
+      <div className="my-10">
+        <Routes>
+          <Route path="/product/:id" element={<PDPContent />} />
+        </Routes>
+      </div>
 
-    <Footer />
-  </div>
+      <Footer />
+    </div>
+  </Router>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- pdp/src/PDPContent.jsx
+++ pdp/src/PDPContent.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-
+import { useParams } from "react-router-dom";
 import { getProductById, currency } from "home/products";
 
 export default function PDPContent() {
-  const id = 1;
+  const { id } = useParams();
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
@@ -20,6 +20,16 @@ export default function PDPContent() {
     <div className="grid grid-cols-2 gap-5">
       <div>
         <img src={product.image} alt={product.name} />
+      </div>
+      <div>
+        <div className="flex">
+          <h1 className="font-bold text-3xl flex-grow">{product.name}</h1>
+          <div className="font-bold text-3xl flex-end">
+            {currency.format(product.price)}
+          </div>
+        </div>
+        <div className="mt-10">{product.description}</div>
+        <div className="mt-10">{product.longDescription}</div>
       </div>
     </div>
   );
