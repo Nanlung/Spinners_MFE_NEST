8ab342f18c85abf81a9de5c8142364368de02b62
Nanlung <nanlung.temlong@gmail.com>
2023-08-03 11:58:23 +0100

    cart feature

---
+++ cart/src/CartContent.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+export default function CartContent() {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {}, []);
+
+  return <div>JWT: {token}</div>;
+}
